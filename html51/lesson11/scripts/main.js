c1affcd9b172b0a8133df6ff9879c60d7d996a92
bohdan78-ops <perevoznyk.bohdan@chnu.edu.ua>
2021-05-05 00:00:58 +0300

    commit

---
+++ html51/lesson11/scripts/main.js
@@ -0,0 +1,10 @@
+function startLogic(){
+    var newHtml = '<em>New Header</em>';
+    document.getElementById('deaderItem').innerHTML = newHtml;
+}
+startLogic();
+function clickHandler() {
+
+    alert('You clicked me!');
+    
+    }
---
+++ html51/lesson12/js.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById('c1')
+var ctx =canvas.getCotext('2d')
+
+ctx.strokeStyle = "green";
+ctx.liteWidth ="10";
+ctx.rect (50,10,10,10);
+ctx.stroke();
